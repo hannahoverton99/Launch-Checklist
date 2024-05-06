f0c6fd4e012ab8fa72b73b69d075e8e7e690da1b
Hannah Overton <hannahoverton99@gmail.com>
2024-05-05 22:22:46 -0500

    last push

--- scriptHelper.js
+++ scriptHelper.js
@@ -7,13 +7,13 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
     const missionTarget= document.getElementById("missionTarget");
 
         missionTarget.innerHTML = `
-    <h2> Mission Destination </h2>
+    <h2>Mission Destination</h2>
     <ol>
-        <li> Name: ${name} </li>
-        <li> Diameter: ${diameter} </li>
-        <li> Star: ${star} </li>
-        <li> Distance from Earth: ${distance} </li>
-        <li> Number of Moons: ${moons} </li>
+        <li>Name: ${name}</li>
+        <li>Diameter: ${diameter}</li>
+        <li>Star: ${star}</li>
+        <li>Distance from Earth: ${distance}</li>
+        <li>Number of Moons: ${moons}</li>
     </ol>
     <img src="${imageUrl}">
  `
